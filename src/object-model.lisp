847825f04c593d7facc2bd1d019d89e0d0fbaa24
takuya9622 <t.takuya9622@gmail.com>
2026-09-21 21:42:42 +0900

    feat: publish precise roots with native ABI v3 frames

--- src/object-model.lisp
+++ src/object-model.lisp
@@ -8,6 +8,10 @@
 (defun symbol-kind (name)
   (cond
     ((keywordp name) :code)
+    ((and (listp name) (= (length name) 2) (eq (first name) :text)
+          (typep (second name) '(integer 0 *))) :text)
+    ((and (listp name) (= (length name) 2) (eq (first name) :helper)
+          (member (second name) '(:text.length :text.equal :text.not-equal :text.concat :text.slice))) :helper)
     ((and (listp name)
           (case (first name)
             (:function (and (= 2 (length name)) (typep (second name) '(integer 0 *))))
